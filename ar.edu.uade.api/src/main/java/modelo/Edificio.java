8dc0fd8dd05c20e9551c0c1b95f6c46788829119
Maxmart27 <Maximmart007@gmail.com>
2023-08-22 10:31:35 -0300

    Edificio, Unidad y EstadoUnidad

--- ar.edu.uade.api/src/main/java/modelo/Edificio.java
+++ ar.edu.uade.api/src/main/java/modelo/Edificio.java
@@ -8,4 +8,12 @@ public class Edificio {
     private String direccion;
     private String administrador;
     private List<Reclamo> reclamo;
+
+    public Edificio(int id, List<Unidad> unidades, String direccion, String administrador, List<Reclamo> reclamo) {
+        this.id = id;
+        this.unidades = unidades;
+        this.direccion = direccion;
+        this.administrador = administrador;
+        this.reclamo = reclamo;
+    }
 }
--- ar.edu.uade.api/src/main/java/modelo/Unidad.java
+++ ar.edu.uade.api/src/main/java/modelo/Unidad.java
@@ -1,8 +1,23 @@
 package modelo;
 
+import java.util.List;
+
 public class Unidad {
     private int id;
     private int piso;
     private int numero;
     private EstadoUnidad estado;
+    private List<Usuario> duenios;
+    private List<Usuario> inquilinos;
+    private List<String> habitantes;
+
+    public Unidad(int id, int piso, int numero, EstadoUnidad estado, List<Usuario> duenios, List<Usuario> inquilinos, List<String> habitantes) {
+        this.id = id;
+        this.piso = piso;
+        this.numero = numero;
+        this.estado = estado;
+        this.duenios = duenios;
+        this.inquilinos = inquilinos;
+        this.habitantes = habitantes;
+    }
 }
