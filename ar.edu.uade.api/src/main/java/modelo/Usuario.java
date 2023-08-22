fd9aaef5ec5045a8a2939789ea9dd241c1e17847
Tomas Molina <molinatomas011@gmail.com>
2023-08-22 10:22:52 -0300

    Usuario.java

---
+++ ar.edu.uade.api/src/main/java/modelo/Usuario.java
@@ -0,0 +1,13 @@
+package modelo;
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private int dni;
+    private String telefono;
+    private String usuario;
+    private String contra;
+    private List<Reclamo> reclamos;
+    private List<Unidad> unidades;
+
+}
