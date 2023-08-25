46aaa0c59f4ade5eb9ec92b978af9ea5cbbe2e30
Cheison <agustinvitale100@gmail.com>
2023-08-25 19:02:34 -0300

    Agregado junit para testing

---
+++ ar.edu.uade.api/src/test/java/ReclamoTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+
+public class ReclamoTest {
+    @Test
+    void test(){
+
+        /* crear reclamo */
+    }
+}
---
+++ ar.edu.uade.api/src/test/java/UsuarioTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+
+
+class UsuarioTest {
+    @Test
+    void test() {
+        /* registrar usuario */
+        /* verificar usuario */
+    };
+}
