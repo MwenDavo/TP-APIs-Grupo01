53e99e6d03108375737aa4aad2eab8414933194f
Cheison <agustinvitale100@gmail.com>
2023-09-27 10:02:28 -0300

    Agregado Reclamo Controller y Reclamo DTO

---
+++ ar.edu.uade.api/src/main/java/application/controller/ReclamoController.java
@@ -0,0 +1,128 @@
+package application.controller;
+
+import application.model.entity.Edificio;
+import application.model.entity.Reclamo;
+import application.model.entity.dto.ReclamoDTO;
+import application.service.IReclamoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/reclamos")
+public class ReclamoController {
+    @Autowired
+    private IReclamoService reclamoService;
+
+    @PostMapping("/reclamo")
+    public ResponseEntity<ReclamoDTO> create(@RequestBody ReclamoDTO reclamoDTO) {
+        Reclamo reclamo = convertToEntity(reclamoDTO);
+        reclamoService.create(reclamo);
+        reclamoDTO = convertToDTO(reclamo);
+        return new ResponseEntity<>(reclamoDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/edificio/{id}")
+    public ResponseEntity<?> read(@PathVariable long id) {
+        Reclamo reclamo = reclamoService.read(id);
+        if (reclamo == null) {
+            String mensaje = "Edificio no encontrado.";
+            return new ResponseEntity<>(mensaje, HttpStatus.NOT_FOUND);
+        }
+        ReclamoDTO reclamoDTO = convertToDTO(reclamo);
+        return new ResponseEntity<>(reclamoDTO, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/edificio/edificioParameters")
+    public ResponseEntity<?> readParameterized(@RequestParam("id") long id) {
+        Reclamo reclamo = reclamoService.read(id);
+        if (reclamo == null) {
+            String mensaje = "Edificio no encontrado.";
+            return new ResponseEntity<>(mensaje, HttpStatus.NOT_FOUND);
+        }
+        ReclamoDTO reclamoDTO = convertToDTO(reclamo);
+        return new ResponseEntity<>(reclamoDTO, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/edificios")
+    public List<ReclamoDTO> readAll(UsuarioDTO usuarioDTO) {
+        List<Reclamo> reclamos = reclamoService.readAll();
+        List<ReclamoDTO> response = new ArrayList<>();
+        for (Reclamo reclamo : reclamos) {
+            ReclamoDTO edificioDTO = convertToDTO(reclamo);
+            response.add(reclamoDTO);
+        }
+        return response;
+    }
+
+    @PutMapping("/edificios/{id}")
+    public ResponseEntity<?> update(@PathVariable long id, @RequestBody ReclamoDTO reclamoDTO) {
+        if (reclamoService.read(id) == null) {
+            String mensaje = "Edificio no encontrado.";
+            return new ResponseEntity<>(mensaje, HttpStatus.NOT_FOUND);
+        }
+        Edificio edificio = convertToEntity(reclamoDTO);
+        reclamoService.update(id, reclamo);
+        reclamoDTO = convertToDTO(reclamo);
+        return new ResponseEntity<>(reclamoDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/edificios/edificioParameters")
+    public ResponseEntity<?> updateParameterized(@RequestParam("id") long id, @RequestBody ReclamoDTO reclamoDTO) {
+        if (reclamoService.read(id) == null) {
+            String mensaje = "Edificio no encontrado.";
+            return new ResponseEntity<>(mensaje, HttpStatus.NOT_FOUND);
+        }
+        Edificio edificio = convertToEntity(reclamoDTO);
+        reclamoService.update(id, reclamo);
+        reclamoDTO = convertToDTO(reclamo);
+        return new ResponseEntity<>(reclamoDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/reclamos/{id}")
+    public ResponseEntity<?> delete(@PathVariable long id) {
+        Reclamo reclamo = reclamoService.read(id);
+        if (reclamo == null) {
+            String mensaje = "Reclamo no encontrado.";
+            return new ResponseEntity<>(mensaje, HttpStatus.NOT_FOUND);
+        }
+        reclamoService.delete(reclamo);
+        String mensaje = "Reclamo eliminado.";
+        return new ResponseEntity<>(mensaje, HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/edificios/edificioParameters")
+    public ResponseEntity<?> deleteParameterized(@RequestParam("id") long id) {
+        Reclamo reclamo = reclamoService.read(id);
+        if (reclamo == null) {
+            String mensaje = "Edificio no encontrado.";
+            return new ResponseEntity<>(mensaje, HttpStatus.NOT_FOUND);
+        }
+        reclamoService.delete(reclamo);
+        String mensaje = "Edificio eliminado.";
+        return new ResponseEntity<>(mensaje, HttpStatus.NO_CONTENT);
+    }
+
+    private ReclamoDTO convertToDTO(Reclamo reclamo) {
+        return new ReclamoDTO(
+                edificio.getDireccion(),
+                edificio.getUnidades(),
+                edificio.getReclamos()
+        );
+    }
+
+
+    private Edificio convertToEntity(ReclamoDTO reclamoDTO) {
+        return new Reclamo(
+                edificioDTO.getDireccion(),
+                edificioDTO.getUnidades(),
+                edificioDTO.getReclamos()
+        );
+    }
+
+
+}
---
+++ ar.edu.uade.api/src/main/java/application/model/entity/dto/ReclamoDTO.java
@@ -0,0 +1,84 @@
+package application.model.entity.dto;
+
+import application.model.entity.Foto;
+import application.model.entity.Log;
+import application.model.entity.Usuario;
+import application.model.util.EstadoReclamo;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReclamoDTO {
+
+    private long id;
+
+    private String descripcion;
+
+    private List<Foto> fotos = new ArrayList<>();
+
+    private Usuario usuario;
+
+    private EstadoReclamo estadoReclamo = EstadoReclamo.NUEVO;
+
+    private List<Log> historial = new ArrayList<>();
+
+    public ReclamoDTO(long id, String descripcion, List<Foto> fotos, Usuario usuario, EstadoReclamo estadoReclamo, List<Log> historial) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.fotos = fotos;
+        this.usuario = usuario;
+        this.estadoReclamo = estadoReclamo;
+        this.historial = historial;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public List<Foto> getFotos() {
+        return fotos;
+    }
+
+    public void setFotos(List<Foto> fotos) {
+        this.fotos = fotos;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public EstadoReclamo getEstadoReclamo() {
+        return estadoReclamo;
+    }
+
+    public void setEstadoReclamo(EstadoReclamo estadoReclamo) {
+        this.estadoReclamo = estadoReclamo;
+    }
+
+    public List<Log> getHistorial() {
+        return historial;
+    }
+
+    public void setHistorial(List<Log> historial) {
+        this.historial = historial;
+    }
+}
