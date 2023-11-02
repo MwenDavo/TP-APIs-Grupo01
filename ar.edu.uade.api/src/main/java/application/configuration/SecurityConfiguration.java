7f5068b076120fd54ec6eba2666a32b7d59f4417
Tomas Molina <molinatomas011@gmail.com>
2023-11-01 23:03:58 -0300

    CORS y otros cambios

--- ar.edu.uade.api/src/main/java/application/configuration/SecurityConfiguration.java
+++ ar.edu.uade.api/src/main/java/application/configuration/SecurityConfiguration.java
@@ -4,12 +4,16 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import javax.crypto.SecretKey;
 
@@ -23,6 +27,7 @@ public class SecurityConfiguration {
                 (authz) -> authz.anyRequest().authenticated())
                 .addFilterBefore(jwtAuth(), UsernamePasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
         ;
         return http.build();
     }
@@ -34,6 +39,22 @@ public class SecurityConfiguration {
         );
     }
 
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        // Permitir solicitudes desde cualquier origen, puedes personalizar esto según tus necesidades.
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedHeader("Authorization");
+        config.addAllowedHeader("Content-Type");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("OPTIONS");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
     @Bean
     public SecretKey secretKey() {
         return Keys.secretKeyFor(SignatureAlgorithm.HS256);
