d244a8ec5aac9c0ab8acd054c828ce9d7f730d2a
matiasfelau <matiasfelau@gmail.com>
2023-11-21 10:23:26 -0300

    una boludé

--- ar.edu.uade.api/src/main/java/application/service/ConverterService.java
+++ ar.edu.uade.api/src/main/java/application/service/ConverterService.java
@@ -254,12 +254,15 @@ public class ConverterService implements IConverterService{
     public List<FotoDTO> convertToDTOf(List<Foto> fotos) throws IOException {
         List<FotoDTO> fotosdto = new ArrayList<>();
         int count = 0;
+        /*
         for (Foto f:fotos) {
             MultipartFile multiPartFile = new MockMultipartFile("foto" + count, f.getData());
             FotoDTO fotoDTO = new FotoDTO(multiPartFile);
             fotosdto.add(fotoDTO);
             count++;
         }
+
+         */
         return fotosdto;
     }
     @Override
